347ef592b19d594404a6fd65aaeffee937445ec0
BabaInformatiker <BabaInformatiker@gmail.com>
2017-02-06 21:14:26 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include "Schiff.h"
+#include "Spielfeld.h"
+
+using namespace std;
+
+int main() {
+    
+   
+    
+    Spielfeld sp;
+    sp.spielen();
+    
+    
+    
+    
+    
+    return 0;
+}
+
